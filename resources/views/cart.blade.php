627fb98d5b84db28ec2f1d70e903671e0b03839a
martinsantax <martinramirosantacruz@gmail.com>
2019-07-31 16:56:00 -0300

    version 9

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,26 +2,49 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/index.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
     <title>Carrito</title>
   </head>
-  <body>
-    <div class="">
-      <ul>
-        @forelse ($carts as $item)
-          <li>Nombre: {{$item->name}} | Cantidad: {{$item->quantity}} |
-          Precio: {{$item->price}} | sub-total: {{$item->price * $item->quantity}}</li>
-          <a href="/delete/{{$item->id}}">Eliminar</a>
-        @empty
-          <p>Carrito vacío</p>
-          <a href="/productos">Ver todos los productos</a>
-        @endforelse
-      </ul>
-     <div class="">
-      <form class="" action="/cartClose" method="post">
-        @csrf
-        <button type="submit" name="button">Comprar</button>
-      </form>
-    </div>
+  <body id="cartBody">
+    	<div class="container">
+
+        <div class="contentAll">
+          <header>
+
+            @include('/layouts/nabvar')
+    				<hr size="1" class="line" />
+
+    						<!-- MENU MOBILE -->
+            @include('/layouts/navbarMobile')
+
+    			 </header>
+    		 </div>
+
+        <div class="mainCart">
+          <ul>
+            @forelse ($carts as $item)
+              <div class="mainLis">
+                <div class="lis">
+                  <li class="cartIl"> Nombre:  {{$item->name}}  | </li>
+                  <li class="cartIl"> Cantidad: {{$item->quantity}}  | </li>
+                  <li class="cartIl"> Precio: $ {{$item->price}}  | </li>
+                  <li class="cartIl"> sub-total: $ {{$item->price * $item->quantity}}</li>
+                </div>
+                <a href="/delete/{{$item->id}}" class="cartDelete">Eliminar</a>
+              </div>
+            @empty
+              <p class="pCart">Carrito vacío</p>
+              <a href="/productos" class="aCart">Ver todos los productos</a>
+            @endforelse
+          </ul>
+         <div class="">
+          <form class="" action="/cartClose" method="post">
+            @csrf
+            <button type="submit" name="button" class="cartBuy">Comprar</button>
+          </form>
+        </div>
+      </div>
     </div>
   </body>
 </html>
--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -41,22 +41,16 @@
 
 				<ul>
 						@foreach ($products as $product)
-							<li>
-							<a href="/product/{{$product->id}}"><img src="/storage/products/{{$product->image}}"></a>
-							<a href="/product/{{$product->id}}">{{$product->name}}</a>
-							<a class="price">${{$product->price}}</a>
-							<a href="/category/{{$product->category_id}}"></a>
-							<span class="neon"></span>
-							<li>
-							<a href="/product/{{$product->id}}"><img src="/storage/products/{{$product->image}}"></a>
-							<a href="/product/{{$product->id}}">{{$product->name}}</a>
-							<a class="price">${{$product->price}}</a>
-							<a href="/category/{{$product->category_id}}"></a>
-							<span class="neon2"></span>
-						</li>
+
+           	<li>
+								<a href="/product/{{$product->id}}"><img src="/storage/products/{{$product->image}}"></a>
+								<a href="/product/{{$product->id}}">{{$product->name}}</a>
+								<a class="price">${{$product->price}}</a>
+								<a href="/category/{{$product->category_id}}"></a>
+					  </li>
 						@endforeach
 				</ul>
-				<br>
+				<hr class="paginationHr">
 				{{$products->links()}}
 			</div>
 		</div>
